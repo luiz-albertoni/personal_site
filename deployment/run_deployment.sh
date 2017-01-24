1be9c583475592634428b2bddd90f1559610fa5b
luiz-albertoni <luiz.albertoni@gmail.com>
2017-01-24 00:04:25 -0200

    add deployment

---
+++ deployment/run_deployment.sh
@@ -0,0 +1,10 @@
+ls
+cd /home/ubuntu/personal_site
+git reset --hard HEAD
+git pull origin stage
+rm -rf vendor
+composer install
+composer dump-autoload
+rm -rf public/build/js/
+rm -rf public/build/css/
+gulp
--- resources/views/emails/send.blade.php
+++ resources/views/emails/send.blade.php
@@ -1,4 +1,4 @@
 <div>
     {{ $mail['body'] }}
-    
+
 </div>
