141b493804a1d00641ec7d7b5eca663773318575
luiz-albertoni <luiz.albertoni@gmail.com>
2017-08-23 15:46:46 -0300

    Add moe space for blog and white background

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -8,7 +8,7 @@
         min-height: 1200px" ng-controller="PostController as vm"  ng-cloak>
 
 
-        <div class="panel-body" style="width:20%; min-height: 1200px; border: 2px solid #e7e7e7; float: left; background-color: #dcf3dc " >
+        <div class="panel-body" style="width:20%; min-height: 2000px; border: 2px solid #e7e7e7; float: left; background-color: #dcf3dc " >
 
            <div style="height: 55px; width: 100%; border-bottom: 2px solid #e7e7e7;">
               <label style="text-align: center; width: 100%;"><a href="{{ route('blog') }}">  Todos os Posts</a> </label>
@@ -41,7 +41,7 @@
 
         </div>
 
-        <div class="panel-body" style="width:80%; min-height: 1200px;float: right;  border: 2px solid #e7e7e7;" >
+        <div class="panel-body" style="width:80%; min-height: 2000px;float: right;  border: 2px solid #e7e7e7; background: white;" >
 
             <div class="ibox-title" style="text-align: center;  margin-bottom: 20px; border-bottom: 1px solid #e7e7e7;">
                 <h2 style="margin-top: 10px">{{ $post->title}}</h2>
